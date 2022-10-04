114c01038fbee64535043229d5218a2a34c93300
socar-chungmyoung <chungmyoung@socar.kr>
2022-10-04 18:58:53 +0900

    init project

---
+++ src/main/kotlin/com/example/navigationreset/NavigationresetApplication.kt
@@ -0,0 +1,11 @@
+package com.example.navigationreset
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NavigationresetApplication
+
+fun main(args: Array<String>) {
+	runApplication<NavigationresetApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/navigationreset/NavigationresetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.navigationreset
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NavigationresetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
